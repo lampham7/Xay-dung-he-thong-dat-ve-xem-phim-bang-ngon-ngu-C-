07e03bac16c94bb6451a1bfe723850366bf995cb
Lam_Pham <77009602+lampham7@users.noreply.github.com>
2022-06-15 02:21:39 +0700

    Update create tables trong database MovieTicketManagementSystem.sql

--- create tables trong database MovieTicketManagementSystem.sql
+++ create tables trong database MovieTicketManagementSystem.sql
@@ -7,37 +7,39 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Customer_SignUp](
-	[ID] [int] NOT NULL,
-	[FullName] [nchar](20) NOT NULL,
-	[UserName] [nchar](10) NOT NULL,
-	[Password] [nchar](10) NOT NULL,
-	[MobileNo] [varchar](50) NOT NULL,
-	[Gender] [nchar](10) NOT NULL,
-	[EmailAddress] [nchar](20) NOT NULL,
-	[Nationality] [nchar](10) NULL
-) ON [PRIMARY]
-GO
-CREATE TABLE [dbo].[Movie_Book](
-	[BookID] [nchar](10) NOT NULL,
-	[Username] [nchar](10) NOT NULL,
-	[Movie_Title] [nvarchar](50) NOT NULL,
-	[TotalCustomer] [nchar](10) NOT NULL,
-	[Type] [nchar](5) NOT NULL,
-	[SelectSit] [nchar](10) NOT NULL,
-	[ShowTime] [nchar](10) NULL,
-	[PriceToPay] [nchar](10) NULL
-) ON [PRIMARY]
-GO
-CREATE TABLE [dbo].[Movie_Information](
-	[Movie_ID] [int] NOT NULL,
-	[Movie_Title] [nvarchar](50) NOT NULL,
-	[Movie_Genre] [nvarchar](50) NOT NULL,
-	[Movie_Synopsis] [nvarchar](max) NOT NULL,
-	[Movie_RunTime] [nchar](10) NULL,
-	[Movie_ReleaseDate] [date] NOT NULL,
-	[Movie_Image] [image] NULL
+CREATE TABLE [dbo].[Movies](
+	[MovieID] [int] IDENTITY(1,1) NOT NULL,
+	[MovieName] [nvarchar](50) NOT NULL,
+	[Genres] [nvarchar](50) NOT NULL,
+	[Synopsis] [nvarchar](max) NOT NULL,
+	[RunTime] [nchar](10) NOT NULL,
+	[ReleaseDate] [date] NOT NULL,
+	[Poster] [image] NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nchar](10) NOT NULL,
+	[Password] [nchar](10) NOT NULL,
+	[FullName] [nchar](20) NOT NULL,
+	[Gender] [nchar](10) NOT NULL,
+	[PhoneNumber] [varchar](50) NOT NULL,
+	[Email] [nchar](20) NOT NULL,
+ CONSTRAINT [PK_Customer_SignUp] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[Bookings](
+	[BookingID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nchar](10) NOT NULL,
+	[MovieName] [nvarchar](50) NOT NULL,
+	[NumberOfSeats] [nchar](10) NOT NULL,
+	[Type] [nchar](10) NOT NULL,
+	[SeatPosition] [nchar](10) NOT NULL,
+	[ShowTime] [nchar](10) NOT NULL,
+	[Price] [nchar](10) NOT NULL
+) ON [PRIMARY]
+
 GO
-
-
